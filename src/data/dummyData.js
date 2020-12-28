1a7b3a877df2d72db3701867f6be39bcb0027450
john lundgren <john.lundgren.lexicon@gmail.com>
2020-12-28 07:50:39 +0100

    Added users to dummy data.

--- src/data/dummyData.js
+++ src/data/dummyData.js
@@ -3,40 +3,70 @@ export const data = [
     user: "john@email.com",
     books: [
       {
-      id: '1',
-      title: 'All the light we can not see',
-      author: 'Anthony Doerr',
-      desc: 'Den franska flickan Marie-Laure har nyligen blivit blind och kan bara tolka krigets fasor genom ljud, dofter och beröring.',
-      image: 'https://buuks.se/images/product_images/swedish_books/9789187441769.jpg'
-    },
-    {
-      id: '2',
-      title: 'Legend',
-      author: 'Marie Lu',
-      desc: `Los Angeles, California Republic of America He is Day. The boy who walks in the light. She is June. The girl who seeks her brother's killer. On the run and undercover, they meet by chance.`,
-      image: 'https://bilder.akademibokhandeln.se/images_akb/9780141339603_383/legend'
-    },
-    {
-      id: '3',
-      title: 'War Cross',
-      author: 'Marie Lu',
-      desc: `For the millions who log in every day, Warcross isn't just a game--it's a way of life.`,
-      image: 'https://bilder.akademibokhandeln.se/images_akb/9781984815767_383/warcross'
-    },
-    {
-      id: '4',
-      title: 'Champion',
-      author: 'Marie Lu',
-      desc: 'June och Day har offrat mycket för medborgarna i Republiken och för varandra. Men det var långt ifrån tillräckligt.',
-      image: 'https://bilder.akademibokhandeln.se/images_akb/9789176455944_383/champion'
-    },
-    {
-      id: '5',
-      title: 'Midnattsstjärnan',
-      author: 'Marie Lu',
-      desc: 'Adelina Amouteru har fått nog av att lida. Hon har vänt ryggen åt dem som förrått henne och uppnått den ultimata hämnden: seger.',
-      image: 'https://bilder.akademibokhandeln.se/images_akb/9789178935093_383/midnattsstjarnan'
-    },
+        id: "1",
+        title: "All the light we can not see",
+        author: "Anthony Doerr",
+        desc:
+          "Den franska flickan Marie-Laure har nyligen blivit blind och kan bara tolka krigets fasor genom ljud, dofter och beröring.",
+        image:
+          "https://buuks.se/images/product_images/swedish_books/9789187441769.jpg",
+      },
+      {
+        id: "2",
+        title: "Legend",
+        author: "Marie Lu",
+        desc: `Los Angeles, California Republic of America He is Day. The boy who walks in the light. She is June. The girl who seeks her brother's killer. On the run and undercover, they meet by chance.`,
+        image:
+          "https://bilder.akademibokhandeln.se/images_akb/9780141339603_383/legend",
+      },
+      {
+        id: "3",
+        title: "War Cross",
+        author: "Marie Lu",
+        desc: `For the millions who log in every day, Warcross isn't just a game--it's a way of life.`,
+        image:
+          "https://bilder.akademibokhandeln.se/images_akb/9781984815767_383/warcross",
+      },
+      {
+        id: "4",
+        title: "Champion",
+        author: "Marie Lu",
+        desc:
+          "June och Day har offrat mycket för medborgarna i Republiken och för varandra. Men det var långt ifrån tillräckligt.",
+        image:
+          "https://bilder.akademibokhandeln.se/images_akb/9789176455944_383/champion",
+      },
+      {
+        id: "5",
+        title: "Midnattsstjärnan",
+        author: "Marie Lu",
+        desc:
+          "Adelina Amouteru har fått nog av att lida. Hon har vänt ryggen åt dem som förrått henne och uppnått den ultimata hämnden: seger.",
+        image:
+          "https://bilder.akademibokhandeln.se/images_akb/9789178935093_383/midnattsstjarnan",
+      },
+    ],
+  },
+  {
+    user: "kalle@hotmail.com",
+    books: [
+      {
+        id: "1",
+        title: "I Am Watching You",
+        author: "Teresa Driscol",
+        desc:
+          "Since that ominous train ride, Ella Longfield has been wracked by guilt. A year after one of her fellow passengers, a teenage girl named Anna",
+        image:
+          "https://m.media-amazon.com/images/G/01/prime/primeinsider2/prnewesttitles/nov18/watching_you._CB480611966_.jpg",
+      },
+      {
+        id: "2",
+        title: "A Case of Need",
+        author: "Michael Crichton",
+        desc: `After a botched medical procedure, the daughter of a heart surgeon dies on the operating table, and his colleague is accused of murder.`,
+        image:
+          "https://m.media-amazon.com/images/G/01/prime/primeinsider2/pageturners/newaugust/case_need._CB471506537_.jpg",
+      },
     ],
   },
 ];
